5e6f0e3f478c84350ab74b6e89c486e26507877d
Martijn Pieters <mj@zopatista.com>
2023-03-04 12:41:28 +0000

    build: specialise each script's homepage URL

--- webpack.config.ts
+++ webpack.config.ts
@@ -81,6 +81,7 @@ const config: (
             : `${downloadUrl}/${data.basename}.user.js`
           const headers = {
             ...HEADER_DEFAULTS,
+            homepage: '[homepage]/scripts/[basename]',
             supportURL: '[bugs]?q=is:issue+is%3Aopen+label:[basename]',
             downloadURL,
             updateURL: isDevMode
