b7a48d8fd18282bb7c84fb7e37d6bbf4489b5735
Martijn Pieters <mj@zopatista.com>
2023-03-21 19:15:56 +0000

    test(connected-users): snapshot tests for users (#569)
    Split out the user module into separate parts and test user HTML rendering with
    snapshots.

---
+++ scripts/connected-users/src/users/controller.ts
@@ -0,0 +1,83 @@
+/* global Stacks */
+import { controllerId } from '../constants'
+
+import { fetchUsers } from './api'
+
+// set padding inside the user cards to 0 in the usercard lists
+// _uc-p is the CSS variable set by Stacks and we can override it here.
+const userStyles = `.s-${controllerId} .s-user-card { --_uc-p: 0 }`
+
+export class UserListController extends Stacks.StacksController {
+  static controllerId = `${controllerId}-user-list`
+
+  static targets = ['count', 'userRow']
+  declare readonly countTarget: HTMLElement
+  declare readonly userRowTargets: HTMLElement[]
+
+  static classes = ['userCard']
+  declare readonly userCardClasses: string
+
+  static afterLoad(identifier: string): void {
+    document.head.insertAdjacentHTML(
+      'beforeend',
+      `<style id="${identifier}-styles">${userStyles}</style>`
+    )
+  }
+
+  connect(): void {
+    this.updateUsers()
+  }
+
+  updateUsers(): void {
+    this.countTarget.innerHTML = this.userRowTargets.length.toString()
+    // replace sparse user cards with full versions
+    // preserves the first child of an existing s-user-card div if the row is
+    // marked with data-user-card-keep-first="true"
+    const hydrationRows = new Map(
+      this.userRowTargets.reduce(
+        (entries, row) =>
+          row.dataset.hydrated === 'true'
+            ? entries
+            : [...entries, [parseInt(row.dataset.uid || '0'), row]],
+        [] as [number, HTMLDivElement][]
+      )
+    )
+    if (hydrationRows.size === 0) return
+    window.requestAnimationFrame(async () => {
+      for await (const user of fetchUsers([...hydrationRows.keys()], true)) {
+        const userRow = hydrationRows.get(user.user_id)
+        if (!userRow) continue
+        const firstChild =
+          userRow.dataset.userCardKeepFirst === 'true'
+            ? userRow
+                .querySelector<HTMLDivElement>('.s-user-card :first-child')
+                ?.cloneNode(true)
+            : null
+        const existingCard =
+          userRow.querySelector<HTMLDivElement>('.s-user-card')
+        if (existingCard) existingCard.outerHTML = user.toHTML()
+        else userRow.innerHTML = user.toHTML()
+        const newCard = userRow.querySelector('.s-user-card')
+        newCard?.classList.add(...this.userCardClasses)
+        if (firstChild) {
+          newCard?.insertAdjacentElement(
+            'afterbegin',
+            firstChild as HTMLElement
+          )
+        }
+        userRow.dataset.hydrated = 'true'
+      }
+      // signal the Mod User Quicklinks Everywhere script
+      this.dispatch('moduserquicklinks', { prefix: '' })
+      this.dispatch('usersHydrated', {
+        detail: this.userRowTargets.reduce(
+          (uids, r) =>
+            r.querySelector('s-user-card__deleted') === null
+              ? [...uids, parseInt(r.dataset.uid || '0')]
+              : uids,
+          [] as number[]
+        ),
+      })
+    })
+  }
+}
---
+++ scripts/connected-users/test/utils.ts
@@ -0,0 +1,22 @@
+type CP<T extends unknown[]> = T extends []
+  ? []
+  : { [i in keyof T]: T[i] extends (infer S)[] ? S : never }[]
+/** Produce the cartesian product of the values of the input arrays */
+export const cartesian = <T extends unknown[][]>(inputs: [...T]) => {
+  const result = [] as CP<T>
+  if (!inputs.length) return result
+  const indexes = inputs.map(() => 0)
+  while (true) {
+    result.push(indexes.map((i, idx) => inputs[idx][i]))
+    let j = indexes.length - 1
+    while (true) {
+      if (indexes[j] < inputs[j].length - 1) {
+        indexes[j] += 1
+        break
+      }
+      indexes[j] = 0
+      j -= 1
+      if (j < 0) return result
+    }
+  }
+}
